1f55c386dbd6bb2bb1bffa64ca0017a1cd5ae2a8
lokmankhodziri <lokmankhodziri@gmail.com>
2025-06-09 22:15:13 +0800

    feat: add active state to sidebar navigation and improve routing

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -4,21 +4,46 @@ import React from 'react'
 import styled from 'styled-components'
 import { useGlobalState } from '../context/globalContextProvider'
 import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname, useRouter } from 'next/navigation'
+
+import menu from '@/app/utils/menu'
 
 const Sidebar = () => {
 
     const {theme} = useGlobalState();
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const handleClick = (link: string) => {
+      router.push(link)
+    }
 
   return (
     <SidebarStyled theme={theme}>
       <div className="profile">
         <div className="profile-overlay"></div>
         <Image width={70} height={70} src="https://avatar.iran.liara.run/public/46" alt="profile"/>
+          <h1>
+          <span>Akon</span>
+          <span>Icon</span>
+        </h1>
       </div>
-      <h1>
-        <span>Akon</span>
-        <span>Icon</span>
-      </h1>
+      <ul className='nav-items'>
+        {menu.map((item) =>{
+
+          const link = item.link;
+
+          return <li className={`nav-item ${pathname === link ? "active" : ""}`} onClick={() => {
+            handleClick(item.link)
+          }}>
+            {item.icon}
+            <Link href={link}>
+              {item.title}
+            </Link>
+          </li>
+        })}
+      </ul>
     </SidebarStyled>
   )
 }
