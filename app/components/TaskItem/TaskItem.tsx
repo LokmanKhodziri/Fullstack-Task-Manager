70c34853fc02116a4eea823722bc250f5bd5bdfb
lokmankhodziri <lokmankhodziri@gmail.com>
2025-06-23 23:05:16 +0800

    Update TaskItem component and API route

--- app/components/TaskItem/TaskItem.tsx
+++ app/components/TaskItem/TaskItem.tsx
@@ -1,20 +1,32 @@
 "use client"
 
+import { edit, trash } from '@/app/utils/Icons';
 import React from 'react';
 
 interface Props {
-    task: any
+    title?: string;
+    description: string;
+    date: string;
+    isCompleted: boolean;
+    id: string
 }
 
-function TaskItem({ task }: Props) {
-
-    const {title, description, date, completed, important} = task;
+function TaskItem({ title, description, date, isCompleted, important, id }: Props) {
 
   return (
     <div>
         <h2>{title}</h2>
         <p>{description}</p>
         <span>{new Date(date).toLocaleDateString()}</span>
+        <div className="task-footer">
+            {isCompleted ? (
+                <button className="completed">Completed</button>
+            ) : (
+                <button className="not-completed">Not Completed</button>
+            )}
+                <button className="edit">{edit}</button>
+                <button className="delete">{trash}</button>
+        </div>
     </div>
   );
 }
